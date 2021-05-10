b0a3bb8d73a0d8f38508874589606b441dd55d72
Ayoub.Zekhnine <ayoub.zekhnine@STDE.EFS.SANTE.BAN>
2021-05-11 00:22:30 +0200

    first commit

---
+++ src/main/java/com/example/apitest/Controller/RestaurantController.java
@@ -0,0 +1,20 @@
+package com.example.apitest.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "restaurant")
+public class RestaurantController {
+
+    @GetMapping("/all")
+    public String afficheText(){
+
+        return "Hellow world";
+
+    }
+
+}
